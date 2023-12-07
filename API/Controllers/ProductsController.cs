1173c8bbce3c4c1232840292d76456118526a383
Avni Şen <senavni35@gmail.com>
2023-12-07 23:30:52 +0300

    first commit new application

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _storeContext;
+    public ProductsController(StoreContext context)
+    {
+        _storeContext = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await _storeContext.Products.ToListAsync();
+        return products;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProducts(int id)
+    {
+        var product = await _storeContext.Products.FirstOrDefaultAsync(x=>x.Id == id);
+        return product;
+    }
+}
